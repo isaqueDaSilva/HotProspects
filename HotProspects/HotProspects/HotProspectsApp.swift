5f84cdc5e338ef96214ef4ef06e15614bb207d89
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-10-05 22:21:52 -0300

    initial commit

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Isaque da Silva on 05/10/23.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
