174c7d09d3be35d35de683b7d7f921f1d212eb16
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-10-07 23:59:10 -0300

    some updates

---
+++ HotProspects/Others/FilterTypesViews.swift
@@ -0,0 +1,8 @@
+//
+//  FilterTypesView.swift
+//  HotProspects
+//
+//  Created by Isaque da Silva on 07/10/23.
+//
+
+import Foundation
---
+++ HotProspects/View/ProspectsView.swift
@@ -0,0 +1,18 @@
+//
+//  ProspectsView.swift
+//  HotProspects
+//
+//  Created by Isaque da Silva on 07/10/23.
+//
+
+import SwiftUI
+
+struct ProspectsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProspectsView()
+}
