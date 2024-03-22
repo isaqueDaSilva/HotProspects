0c1431047d385efd8b4b3ed8e4a38e3cadf88fe2
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-03-21 22:22:41 -0300

    New Project

---
+++ HotProspect/HotProspect/HotProspectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectApp.swift
+//  HotProspect
+//
+//  Created by Isaque da Silva on 21/03/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HotProspect/HotProspect/View/HomeView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HotProspect
+//
+//  Created by Isaque da Silva on 21/03/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    HomeView()
+}
