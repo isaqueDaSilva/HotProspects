b9fefc58d0965b87d6bb20ea24e2191cf972583e
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-03-22 22:50:15 -0300

    I created the ProfileView

--- HotProspect/HotProspect/View/ProfileView.swift
+++ HotProspect/HotProspect/View/ProfileView.swift
@@ -9,7 +9,36 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                Section {
+                    NavigationLink(value: "abc") {
+                        LabeledContent {
+                            Icons.qrCode.systemImage
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 35, height: 35)
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                        } label: {
+                            VStack(alignment: .leading) {
+                                Text("Name")
+                                    .bold()
+                                    .lineLimit(1)
+                                Text("email")
+                                    .foregroundStyle(.secondary)
+                            }
+                        }
+                    }
+                }
+                
+                Section {
+                    NavigationLink(value: "abc") {
+                        LabeledContent("Contacted with me:", value: "5")
+                    }
+                }
+            }
+            .navigationTitle("Profile")
+        }
     }
 }
 
