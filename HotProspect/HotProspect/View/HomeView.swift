33a3aa3def1e3de92dc14d55f9cd749b689bd0e7
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-03-22 22:52:41 -0300

    I created the HomeView for store a TabView with the all Prospect Views and ProfileView

--- HotProspect/HotProspect/View/HomeView.swift
+++ HotProspect/HotProspect/View/HomeView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            ForEach(ProspectTagView.allCases) { prospect in
+                ProspectView(prospectType: prospect)
+                    .tabItem {
+                        Label(prospect.rawValue, systemImage: prospect.icon)
+                    }
+            }
+            
+            ProfileView()
+                .tabItem {
+                    Label("Profile", systemImage: Icons.personCrop.rawValue)
+                }
         }
-        .padding()
     }
 }
 
