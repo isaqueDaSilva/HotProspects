b06513f21496cbf4d24800b9f3ac47f6fd8b6ede
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-03-22 22:50:01 -0300

    I created the ProspectView

--- HotProspect/HotProspect/View/ProspectView.swift
+++ HotProspect/HotProspect/View/ProspectView.swift
@@ -8,11 +8,73 @@
 import SwiftUI
 
 struct ProspectView: View {
+    let prospectType: ProspectTagView
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                // Check if has some prospect save
+                
+                // otherwise return ContentUnavailableView
+                ContentUnavailableView(
+                    prospectType.contentUnavaiableTitle,
+                    systemImage: prospectType.contentUnavaiableIcon,
+                    description: prospectType.contentUnavaibleDescription
+                )
+            }
+            .navigationTitle(prospectType.rawValue)
+            .toolbar {
+                ToolbarItem {
+                    HStack {
+                        Button(action: {
+                           
+                        }, label: {
+                            Label("Scan a QR Code", systemImage: Icons.qrCode.rawValue)
+                        })
+                        
+                        Menu {
+                            
+                        } label: {
+                            Image(systemName: Icons.line3Horizontal.rawValue)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+// MARK: Prospect View Populated
+
+extension ProspectView {
+    @ViewBuilder
+    func ProspectViewPopilated() -> some View {
+        List(0..<10) { integer in
+            VStack(alignment: .leading) {
+                Text("Name \(integer + 1)")
+                    .bold()
+                    .lineLimit(1)
+                Text("email \(integer + 1)")
+                    .foregroundStyle(.secondary)
+            }
+            .swipeActions {
+                Button {
+                    // Do something
+                } label: {
+                    Label("Contacted me", systemImage: "person.crop.circle.badge.checkmark")
+                }
+                .tint(.green)
+                
+                Button {
+                    // Do something...
+                } label: {
+                    Label("Notify me", systemImage: "bell.fill")
+                }
+                .tint(.orange)
+            }
+        }
     }
 }
 
 #Preview {
-    ProspectView()
+    ProspectView(prospectType: .uncontacteds)
 }
