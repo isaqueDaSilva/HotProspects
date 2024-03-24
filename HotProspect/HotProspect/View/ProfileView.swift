e5dddb05f08280b9af856040a487bd9ff948006e
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-03-24 03:07:50 -0300

    I created the UserLogged method, for display the user informations when exist an User Logged and a NoUserLogged for display a Button with a route for user made a login or create account.

--- HotProspect/HotProspect/View/ProfileView.swift
+++ HotProspect/HotProspect/View/ProfileView.swift
@@ -8,27 +8,22 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @State private var showingConfirmationDialog = false
+    @State private var showingCreateAccountAlert  = false
+    @State private var showingLoginAlert  = false
+    
+    @State private var email = ""
+    @State private var password = ""
+    @State private var confirmPassword = ""
+    
     var body: some View {
         NavigationStack {
             List {
                 Section {
-                    NavigationLink(value: "abc") {
-                        LabeledContent {
-                            Icons.qrCode.systemImage
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 35, height: 35)
-                                .frame(maxWidth: .infinity, alignment: .trailing)
-                        } label: {
-                            VStack(alignment: .leading) {
-                                Text("Name")
-                                    .bold()
-                                    .lineLimit(1)
-                                Text("email")
-                                    .foregroundStyle(.secondary)
-                            }
-                        }
-                    }
+                    // User Logged
+                    UserLogged()
+                    // Case no user has logged.
+                    NoUserLogged()
                 }
                 
                 Section {
@@ -36,8 +31,108 @@ struct ProfileView: View {
                         LabeledContent("Contacted with me:", value: "5")
                     }
                 }
+                
+                Section {
+                    NavigationLink(value: "abc") {
+                        Text("Notifications")
+                    }
+                } footer: {
+                    Text(Texts.notificationFooterTextProfileView.rawValue)
+                }
+                
+                Section {
+                    Button("Turn a Host") {
+                        
+                    }
+                } footer: {
+                    Text(Texts.turnAHostFooterTextProfileView.rawValue)
+                }
+                
+                Section {
+                    Button("Sign Out", role: .destructive) {
+                        
+                    }
+                }
             }
-            .navigationTitle("Profile")
+            .navigationTitle("Account")
+            .confirmationDialog(
+                "Choice an Action",
+                isPresented: $showingConfirmationDialog
+            ) {
+                Button {
+                    showingLoginAlert = true
+                } label: {
+                    Text("Login")
+                }
+                
+                Button {
+                    showingCreateAccountAlert = true
+                } label: {
+                    Text("Create an Account")
+                }
+            }
+            .alert(
+                "Login",
+                isPresented: $showingLoginAlert
+            ) {
+                TextField("Email", text: $email)
+                TextField("Pasword", text: $password)
+                
+                Button("Login") {}
+                
+                Button("Cancel", role: .cancel) {}
+            }
+        }
+        .alert(
+            "Create an Account",
+            isPresented: $showingCreateAccountAlert
+        ) {
+            TextField("Email", text: $email)
+            TextField("Pasword", text: $password)
+            TextField("Confirm Pasword", text: $confirmPassword)
+            
+            Button("Create") { }
+            
+            Button("Cancel", role: .cancel) { }
+        }
+    }
+}
+
+// MARK: - No user Logged
+extension ProfileView {
+    @ViewBuilder
+    func NoUserLogged() -> some View {
+        Button {
+            showingConfirmationDialog = true
+        } label: {
+            Text("No User Logged.")
+        }
+        .frame(maxWidth: .infinity)
+    }
+}
+
+// MARK: - User Logged
+
+extension ProfileView {
+    @ViewBuilder
+    func UserLogged() -> some View {
+        NavigationLink(value: "abc") {
+            LabeledContent {
+                Icons.qrCode.systemImage
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 35, height: 35)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+            } label: {
+                VStack(alignment: .leading) {
+                    Text("Name")
+                        .bold()
+                        .lineLimit(1)
+                    Text("email")
+                        .font(.subheadline)
+                        .foregroundStyle(.secondary)
+                }
+            }
         }
     }
 }
