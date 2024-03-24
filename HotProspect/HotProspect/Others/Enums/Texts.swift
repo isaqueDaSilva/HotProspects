303fb1c9c21bece2abae030b8e7817a8f6ea919d
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-03-24 03:03:53 -0300

    I created this enum for stores the text for uses in entire App

---
+++ HotProspect/HotProspect/Others/Enums/Texts.swift
@@ -0,0 +1,13 @@
+//
+//  Texts.swift
+//  HotProspect
+//
+//  Created by Isaque da Silva on 23/03/24.
+//
+
+import Foundation
+
+enum Texts: String {
+    case notificationFooterTextProfileView = "Customize how you recive updates about your account."
+    case turnAHostFooterTextProfileView = "Turn a host for other users connect with you."
+}
